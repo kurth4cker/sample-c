8f0543253dcf2de5d5979f99d80823a0cd45f02a
kurth4cker <kurth4cker@gmail.com>
2025-04-16 23:46:39 +0300

    Initialize.

---
+++ main.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2025 kurth4cker <kurth4cker@gmail.com>
+
+#include <stdio.h>
+
+int
+main(void)
+{
+	printf("hello world\n");
+}
---
+++ nob.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+#define NOB_IMPLEMENTATION
+#define NOB_EXPERIMENTAL_DELETE_OLD
+#include "nob.h"
+
+int
+main(int argc, const char **argv)
+{
+	NOB_GO_REBUILD_URSELF_PLUS(argc, argv, "nob.h");
+
+	Nob_Cmd cmd = { 0 };
+	nob_cmd_append(&cmd, "cc", "-std=c17");
+	nob_cmd_append(&cmd, "-g");
+	nob_cmd_append(&cmd, "-Wall", "-Wextra");
+	nob_cmd_append(&cmd, "-Werror");
+	nob_cmd_append(&cmd, "-o", "sample");
+	nob_cmd_append(&cmd, "main.c");
+
+	if (!nob_cmd_run_sync(cmd)) {
+		exit(EXIT_FAILURE);
+	}
+}
