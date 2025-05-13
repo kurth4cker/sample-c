b1df93beaa3bd563479d9aff5c98449c2f0820f9
kurth4cker <kurth4cker@gmail.com>
2025-05-13 15:11:08 +0300

    build: add run subcommand

--- nob.c
+++ nob.c
@@ -9,16 +9,23 @@ main(int argc, const char **argv)
 {
 	NOB_GO_REBUILD_URSELF_PLUS(argc, argv, "nob.h");
 
-	Nob_Cmd cmd = { 0 };
-	nob_cmd_append(&cmd, "cc", "-std=c17");
-	nob_cmd_append(&cmd, "-g");
-	nob_cmd_append(&cmd, "-pedantic");
-	nob_cmd_append(&cmd, "-Wall", "-Wextra");
-	nob_cmd_append(&cmd, "-Werror");
-	nob_cmd_append(&cmd, "-o", "sample");
-	nob_cmd_append(&cmd, "main.c");
+	Nob_Cmd *cmd = &(Nob_Cmd){ 0 };
+	nob_cmd_append(cmd, "cc", "-std=c17");
+	nob_cmd_append(cmd, "-g");
+	nob_cmd_append(cmd, "-pedantic");
+	nob_cmd_append(cmd, "-Wall", "-Wextra");
+	nob_cmd_append(cmd, "-Werror");
+	nob_cmd_append(cmd, "-o", "sample");
+	nob_cmd_append(cmd, "main.c");
 
-	if (!nob_cmd_run_sync(cmd)) {
+	if (!nob_cmd_run_sync_and_reset(cmd)) {
 		exit(EXIT_FAILURE);
+	}
+
+	if (argc == 2 && strcmp(argv[1], "run") == 0) {
+		nob_cmd_append(cmd, "./sample");
+		if (!nob_cmd_run_sync_and_reset(cmd)) {
+			exit(EXIT_FAILURE);
+		}
 	}
 }
